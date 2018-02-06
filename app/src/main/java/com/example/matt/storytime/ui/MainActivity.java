efddd14a5ae80ef04102de8d0810d05444c8e8ff
Matt Whitley <hockeytowndev@gmail.com>
2018-02-06 11:11:42 -0500

    Initial commit

---
+++ app/src/main/java/com/example/matt/storytime/ui/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.matt.storytime.ui;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.matt.storytime.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameField;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        nameField = (EditText) findViewById(R.id.nameEditText);
+
+        Button startButton = (Button) findViewById(R.id.startButton);
+
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = nameField.getText().toString();
+
+                startStory(name);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        nameField.setText("");
+    }
+
+    private void startStory(String name) {
+
+        Intent intent = new Intent(this, StoryActivity.class);
+        Resources resources = getResources();
+        String key = resources.getString(R.string.key_name);
+        intent.putExtra(key, name);
+        startActivity(intent);
+
+        nameField.getText().toString();
+
+    }
+}
